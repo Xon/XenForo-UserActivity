ef75b721e8ee966fe4d19892877eb2edc7836823
Xon <tazipper1@hotmail.com>
2016-04-22 20:35:06 +0800

    Trim the word 'thread' out of places

--- upload/library/SV/UserActivity/XenForo/ControllerPublic/Thread.php
+++ upload/library/SV/UserActivity/XenForo/ControllerPublic/Thread.php
@@ -17,8 +17,9 @@ class SV_UserActivity_XenForo_ControllerPublic_Thread extends XFCP_SV_UserActivi
             $visitor = XenForo_Visitor::getInstance();
             if ($visitor->hasPermission('RainDD_UA_PermissionsMain', 'RainDD_UA_ThreadViewers'))
             {
-                $response->params['RainDD_UA_ThreadUsersViewing'] = $this->_getSVUserActivityModel()->getUsersViewing('thread', $response->params['thread']['thread_id'], $visitor->toArray());
-                $response->params['RainDD_UA_ThreadViewerPermission'] = !empty($response->params['RainDD_UA_ThreadUsersViewing']);
+                $response->params['UA_UsersViewing'] = $this->_getSVUserActivityModel()->getUsersViewing('thread', $response->params['thread']['thread_id'], $visitor->toArray());
+                $response->params['UA_ViewerPermission'] = !empty($response->params['UA_UsersViewing']);
+                $response->params['UA_ContentType'] = new XenForo_Phrase('conversation');
             }
         }
         return $response;
