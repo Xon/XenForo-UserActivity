558174e68ea74ddea71ac864610d3263fce99f63
Xon <tazipper1@hotmail.com>
2017-01-17 12:07:14 +0800

    Force post activity for known actions

--- upload/library/SV/UserActivity/XenForo/ControllerPublic/Post.php
+++ upload/library/SV/UserActivity/XenForo/ControllerPublic/Post.php
@@ -10,9 +10,23 @@ class SV_UserActivity_XenForo_ControllerPublic_Post extends XFCP_SV_UserActivity
         return parent::_preDispatch($action);
     }
 
+    public function canUpdateSessionActivity($controllerName, $action, &$newState)
+    {
+        $actionL = strtolower($action);
+        switch($actionL)
+        {
+            case 'like':
+            case 'rate':
+            case 'threadmark':
+                return true;
+        }
+
+        return parent::canUpdateSessionActivity($controllerName, $action, $newState);
+    }
+
     public function updateSessionActivity($controllerResponse, $controllerName, $action)
     {
-        if ($controllerResponse instanceof XenForo_ControllerResponse_View)
+        if ($controllerResponse instanceof XenForo_ControllerResponse_View && $this->_request->getParam('thread_id') === null)
         {
             if (isset($controllerResponse->params['post']['thread_id']))
             {
