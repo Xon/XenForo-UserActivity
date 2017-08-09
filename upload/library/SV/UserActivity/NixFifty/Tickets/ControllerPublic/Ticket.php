8f30dc45173f52ef182d2bdb062d202666d64f64
Xon <tazipper1@hotmail.com>
2017-08-10 00:57:07 +0800

    NixFifty Ticket's support

---
+++ upload/library/SV/UserActivity/NixFifty/Tickets/ControllerPublic/Ticket.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('SV/UserActivity/ActivityInjector.php');
+class SV_UserActivity_NixFifty_Tickets_ControllerPublic_Ticket extends XFCP_SV_UserActivity_NixFifty_Tickets_ControllerPublic_Ticket
+{
+    protected $activityInjector = [
+        'controller' => 'NixFifty_Tickets_ControllerPublic_Ticket',
+        'type' => 'ticket',
+        'id' => 'ticket_id',
+        'actions' => ['view'],
+    ];
+    use ActivityInjector;
+}
