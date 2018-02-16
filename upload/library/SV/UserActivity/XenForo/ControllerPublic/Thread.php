7267332d7dab9a185f0a8eb368c65f5c4180cf0f
Xon <tazipper1@hotmail.com>
2018-02-17 00:04:35 +0800

    Support Similar threads from ESE

--- upload/library/SV/UserActivity/XenForo/ControllerPublic/Thread.php
+++ upload/library/SV/UserActivity/XenForo/ControllerPublic/Thread.php
@@ -2,6 +2,8 @@
 
 /** @noinspection PhpIncludeInspection */
 include_once('SV/UserActivity/UserActivityInjector.php');
+/** @noinspection PhpIncludeInspection */
+include_once('SV/UserActivity/UserCountActivityInjector.php');
 class SV_UserActivity_XenForo_ControllerPublic_Thread extends XFCP_SV_UserActivity_XenForo_ControllerPublic_Thread
 {
      public function actionIndex()
@@ -20,6 +22,40 @@ class SV_UserActivity_XenForo_ControllerPublic_Thread extends XFCP_SV_UserActivi
 
         return $response;
     }
+
+
+    protected function similarThreadFetcher(
+        /** @noinspection PhpUnusedParameterInspection */
+        XenForo_ControllerResponse_View $response,
+        $action,
+        array $config)
+
+    {
+        if (empty($response->params['svSimilarThreads']))
+        {
+            return null;
+        }
+        $threadIds = [];
+        foreach($response->params['svSimilarThreads'] as $content)
+        {
+            if ($content['content_type'] === 'thread')
+            {
+                $threadIds[] = $content['content_id'];
+            }
+        }
+
+        return $threadIds;
+    }
+
+    protected $countActivityInjector = [
+        [
+            'activeKey' => 'similar-threads',
+            'type'      => 'thread',
+            'actions'   => ['index'],
+            'fetcher'   => 'similarThreadFetcher'
+        ],
+    ];
+    use UserCountActivityInjector;
 
     protected $activityInjector = [
         'controller' => 'XenForo_ControllerPublic_Thread',
