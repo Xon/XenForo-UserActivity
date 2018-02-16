fc21b6a8420709b7ada6e8e2f1ae2d447f6e4c13
Xon <tazipper1@hotmail.com>
2018-02-16 22:37:35 +0800

    Support conversation counts on conversation lists

--- upload/library/SV/UserActivity/XenForo/ControllerPublic/Conversation.php
+++ upload/library/SV/UserActivity/XenForo/ControllerPublic/Conversation.php
@@ -2,8 +2,35 @@
 
 /** @noinspection PhpIncludeInspection */
 include_once('SV/UserActivity/UserActivityInjector.php');
+/** @noinspection PhpIncludeInspection */
+include_once('SV/UserActivity/UserCountActivityInjector.php');
 class SV_UserActivity_XenForo_ControllerPublic_Conversation extends XFCP_SV_UserActivity_XenForo_ControllerPublic_Conversation
 {
+    protected function conversationFetcher(
+        /** @noinspection PhpUnusedParameterInspection */
+        XenForo_ControllerResponse_View $response,
+        $action,
+        array $config)
+
+    {
+        if (empty($response->params['conversations']))
+        {
+            return null;
+        }
+
+        return array_keys($response->params['conversations']);
+    }
+
+    protected $countActivityInjector = [
+        [
+            'activeKey' => 'conversation',
+            'type'      => 'conversation',
+            'actions'   => ['index', 'starred', 'yours', 'starred'],
+            'fetcher'   => 'conversationFetcher'
+        ],
+    ];
+    use UserCountActivityInjector;
+
     protected $activityInjector = [
         'controller' => 'XenForo_ControllerPublic_Conversation',
         'type'       => 'conversation',
